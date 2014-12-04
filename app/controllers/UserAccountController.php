950eaf3a62d6f3e14be2ddb27bb87bfc8a4bb7b7
tianling <2507073658@qq.com>
2014-12-04 15:40:30 +0800

    d

--- app/controllers/UserAccountController.php
+++ app/controllers/UserAccountController.php
@@ -4,5 +4,9 @@
 **createTime:14-12-4 上午12:03
 */
 class UserAccountController extends BaseController{
-    
+    public $uid;
+
+    public function userSite(){
+        $this->uid = Auth::user()->front_uid;
+    }
 }
