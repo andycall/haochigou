bc358abf789a61d935fb5ccbddfb511a54d2ef7f
haoflynet <haoflynet@gmail.com>
2014-12-16 23:10:04 +0800

    unlogin cart

--- app/controllers/ShopController.php
+++ app/controllers/ShopController.php
@@ -73,12 +73,15 @@ class ShopController extends BaseController {
 	 * 目前只实现了已经登录用户的添加
 	 */
 	public function addToCart(){
-		$user = Auth::user();
 		$menu_id = Input::get('good_id');
 		$shop_id = Input::get('shop_id');
 
-		$cartkey = md5($user->front_uid, $user->uid);
-		$key = 'laravel:user:cart'.$cartkey;
+		if( Auth::check() ){
+			$cartkey = Auth::user()->front_uid;
+		}else{
+			$cartkey = $this->getIP();
+		}
+		$key = 'laravel:user:'.$cartkey.':cart';
 
 		// 第一个元素为店铺的ID，购物车里只能放一个店铺的东西
 		if( Redis::llen($key) == 0){
@@ -135,9 +138,12 @@ class ShopController extends BaseController {
 	 * 清空购物车
 	 */
 	public function cartClear(){
-		$user    = Auth::user();
-		$cartkey = md5($user->front_uid, $user->uid);
-		$key     = 'laravel:user:cart'.$cartkey;
+		if( Auth::check() ){
+			$cartkey = Auth::user()->front_uid;
+		}else{
+			$cartkey = $this->getIP();
+		}
+		$key = 'laravel:user:'.$cartkey.':cart';
 		if( Redis::del($key) ){
 			return Response::json(array(
 				'success' => 'true'
@@ -149,9 +155,12 @@ class ShopController extends BaseController {
 	 * 从购物车删除
 	 */
 	public function cartDel(){
-		$user    = Auth::user();
-		$cartkey = md5($user->front_uid, $user->uid);
-		$key     = 'laravel:user:cart'.$cartkey;
+		if( Auth::check() ){
+			$cartkey = Auth::user()->front_uid;
+		}else{
+			$cartkey = $this->getIP();
+		}
+		$key = 'laravel:user:'.$cartkey.':cart';
 
 		$good_id = Input::get('good_id');
 		$shop_id = Redis::lrange($key, 0, 0);
@@ -172,9 +181,12 @@ class ShopController extends BaseController {
 	 * 购物车初始化
 	 */
 	public function cartInit(){
-		$user    = Auth::user();
-		$cartkey = md5($user->front_uid, $user->uid);
-		$key     = 'laravel:user:cart'.$cartkey;
+		if( Auth::check() ){
+			$cartkey = Auth::user()->front_uid;
+		}else{
+			$cartkey = $this->getIP();
+		}
+		$key = 'laravel:user:'.$cartkey.':cart';
 
 		//var_dump(Redis::lrange($key, 0, -1));
 		$shop_id = Redis::lrange($key, 0, 0);
@@ -202,9 +214,12 @@ class ShopController extends BaseController {
 	 * 此项操作必须是购物车至少有一件的情况
 	 */
 	public function cartSetCount(){
-		$user    = Auth::user();
-		$cartkey = md5($user->front_uid, $user->uid);
-		$key     = 'laravel:user:cart'.$cartkey;
+		if( Auth::check() ){
+			$cartkey = Auth::user()->front_uid;
+		}else{
+			$cartkey = $this->getIP();
+		}
+		$key = 'laravel:user:'.$cartkey.':cart';
 
 		$good_id = Input::get('good_id');
 		$shop_id = Input::get('shop_id');	// 不用
@@ -321,6 +336,7 @@ class ShopController extends BaseController {
 			$goods           = Menu::where('shop_id', $shop_id)->where('group_id', $group->id)->get();
 			$classify_images = array();
 			$classify_goods  = array();
+			$j = $k = 0;
 			foreach($goods as $good){
 				$onegood = array();				
 
@@ -356,7 +372,7 @@ class ShopController extends BaseController {
 			$result[$i++] = $one;
 			//array_push($result, $one);
 		}
-		//var_dump($result);
+//		var_dump($result);
 		return $result;
 	}
 
@@ -396,7 +412,7 @@ class ShopController extends BaseController {
 			$one['classify_name']      = $group->name;
 			$one['classify_name_abbr'] = (mb_strlen($group->name, 'utf8') > 10) ? mb_substr($group->name, 0, 3, 'utf8').'...' : $group->name;
 			$one['classify_id']        = $group->id;
-			$one['classify_count']     = Menu::where('shop_id', $shop_id)->where('group_id', $group->activity_id)->get()->count('shop_id');
+			$one['classify_count']     = Menu::where('shop_id', $shop_id)->where('group_id', $group->id)->get()->count('shop_id');
 			$one['classify_icon']      = $group->icon;
 			array_push($goods_category, $one);
 
@@ -678,9 +694,12 @@ class ShopController extends BaseController {
     }
 
 	public function getUserBarCart(){
-		$user = Auth::user();
-		$cartkey = md5($user->front_uid, $user->uid);
-		$key = 'laravel:user:cart'.$cartkey;
+		if( Auth::check() ){
+			$cartkey = Auth::user()->front_uid;
+		}else{
+			$cartkey = $this->getIP();
+		}
+		$key = 'laravel:user:'.$cartkey.':cart';
 
 		if( $shop_id = Redis::lindex($key, 0)){
 			$data['successs'] = 'true';
