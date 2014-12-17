e6da79fb3a85ec888a5e068ae34cae6104ea835b
haoflynet <haoflynet@gmail.com>
2014-12-17 19:23:54 +0800

    favor

--- public/js/template/shop/port.js
+++ public/js/template/shop/port.js
@@ -1,1 +1,1 @@
-define(function(){return{cartSetCount:"./cartSetCount",cartClear:"./cartClear",cartAdd:"addtocart",cartDel:"./cartDel",cartInit:"cartInit",shopFavor:"/shopFavor",delShopFavor:"/delShopFavor"}});
+define(function(){return{cartSetCount:"./cartSetCount",cartClear:"./cartClear",cartAdd:"addtocart",cartDel:"./cartDel",cartInit:"cartInit",shopFavor:"collectshop",delShopFavor:"cancelshop"}});
