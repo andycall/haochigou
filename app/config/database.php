e89044fcd4136d2b1ff53c16cf9c61c0fd8e8c2b
haoflynet <haoflynet@gmail.com>
2014-12-17 08:51:02 +0800

    fix main page bug

--- app/config/database.php
+++ app/config/database.php
@@ -52,18 +52,18 @@ return array(
 			'prefix'   => '',
 		),
 
-//		'mysql' => array(
-//			'driver'    => 'mysql',
-//			'host'      => '42.62.101.36',
-//			'database'  => 'haochigo',
-//			'username'  => 'haochigo',
-//			'password'  => 'vector2014',
-//			'charset'   => 'utf8',
-//			'collation' => 'utf8_unicode_ci',
-//			'prefix'    => 'v_',
-//		),
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => '42.62.101.36',
+			'database'  => 'haochigo',
+			'username'  => 'haochigo',
+			'password'  => 'vector2014',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => 'v_',
+		),
 
-
+/*
         'mysql' => array(
             'driver'    => 'mysql',
             'host'      => '127.0.0.1',
@@ -74,7 +74,7 @@ return array(
             'collation' => 'utf8_unicode_ci',
             'prefix'    => 'v_',
         ),
-
+*/
 		'pgsql' => array(
 			'driver'   => 'pgsql',
 			'host'     => 'localhost',
