ea2bc89b4e522b5aed1f8271f710ffb9fb25d6bf
lijinxin <smelrain@gmail.com>
2014-12-18 12:19:42 +0800

    fix

--- app/views/widget/login_form/login_form.js
+++ app/views/widget/login_form/login_form.js
@@ -201,12 +201,12 @@ define(['jquery','login/port', 'loginPort'], function($, port, loginPort){
                 if(res.success){
                     alert("登陆成功!");
                     location.href = loginPort['jump_port']
-                }else if( res.inutMsg){
-                    alert(res.inputMsg);
-                }else if(res.otherMsg){
+                }else if( res.errMsg.inutMsg){
+                    alert(res.errMsg.inputMsg);
+                }else if(res.errMsg.otherMsg){
                     alert(res.otherMsg);
                 }else{
-                    alert("注册失败!!!");
+                    alert("登陆失败!!!");
                 }
             }
         });
