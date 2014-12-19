e550358ff398180e86c149e59d7ab33b084a8014
lijinxin <smelrain@gmail.com>
2014-12-19 13:33:34 +0800

    fix

--- public/js/widget/pop_window/pop_window.js
+++ public/js/widget/pop_window/pop_window.js
@@ -73,9 +73,9 @@ define([ "jquery", "shop/port" ], function($, port) {
         var $this = $(this);
         $popWindow.css("left", "0px"), $windowMask.show();
         var data = {
-            good_id: $this.parents(".js-get-good-id").attr("data-good_id")
+            goods_id: $this.parents(".js-get-good-id").attr("data-good_id")
         };
-        goodInfo.goods_id = data.good_id, goodInfo.goods_name = $this.parents(".menu_sec_status").siblings(".menu_sec_info").find(".menu_sec_desc").text(), 
+        goodInfo.goods_id = data.goods_id, goodInfo.goods_name = $this.parents(".menu_sec_status").siblings(".menu_sec_info").find(".menu_sec_desc").text(), 
         goodInfo.goods_price = $this.parents(".menu_sec_status").siblings(".menu_sec_action").find(".symbol-rmb").text(), 
         ajaxGetConmments(data);
     }), //关闭左侧框
