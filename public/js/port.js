ef1397e87089d198c810f33fb7f20a7387817199
lijinxin <smelrain@gmail.com>
2014-12-19 22:32:53 +0800

    fix

--- public/js/port.js
+++ public/js/port.js
@@ -1,1 +1,26 @@
-define(function(){return{getRestaurant:"/mapSearch"}});
+/**
+ * 商家页面Ajax 接口
+ */
+define(function() {
+    return {
+        cartSetCount: "/cartSetCount",
+        //购物车设置商品数量
+        cartClear: "/cartClear",
+        //购物车清空
+        cartAdd: "/addtocart",
+        //购物车根据 id 添加商品
+        cartDel: "/cartDel",
+        //购物车根据 id 删除商品
+        cartInit: "/cartInit",
+        //购物车初始化
+        shopFavor: "/collectshop",
+        //收藏店铺
+        delShopFavor: "/cancelshop",
+        //取消收藏商品
+        goodFavor: "/collectmenu",
+        //收藏商品
+        delGoodFavor: "/cancelmenu",
+        //取消收藏商品
+        getComments: "/goods_comments"
+    };
+});
